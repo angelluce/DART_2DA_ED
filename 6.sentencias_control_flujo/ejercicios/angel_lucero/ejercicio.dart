720b0e74f07ad327c5deb19d55872a97eaccf2e1
angelluce <angelluce244@gmail.com>
2024-06-04 18:41:42 -0500

    push

---
+++ 6.sentencias_control_flujo/ejercicios/angel_lucero/ejercicio.dart
@@ -0,0 +1,43 @@
+import 'dart:io';
+
+void main() {
+  /*
+  print('Ingresa un texto');
+  final retorno = stdin.readLineSync() ?? '';
+  if (retorno.toString().toLowerCase() == 'hola') {
+    print('Bienvenido\nIngrese su edad');
+    final edadStr = stdin.readLineSync() ?? '';
+    final edad = int.tryParse(edadStr) ?? 0;
+    print(edad >= 18 ? 'Mayor de edad' : 'Menor de edad');
+  } else {
+    print('Salude >:v');
+  }
+  */
+
+  // for (var i = 0; i <= 10; i++) {
+  // print(i);
+  // }
+
+  // final collection = ['negro', 'blanco'];
+
+  // for (var i = 0; i < collection.length; i++) {
+  //   print('for: ${collection[i]}');
+  // }
+  // for (var element in collection) {
+  //   print('for in: $element');
+  // }
+  // collection.forEach((element) {
+  //   print('forEach: $element');
+  // });
+  // collection.map((e) => 
+  //   print('map: $e')
+  // ).toList();
+  
+
+  tester(null);
+}
+
+
+void tester(String? word) {
+  print(word);
+}
